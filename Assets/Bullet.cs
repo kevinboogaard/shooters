14461130f6abdced4e04380338b4da00d02f6c7b
kevinboogaard <kevin.boogaard3@gmail.com>
2014-09-30 19:50:21 +0200

    add
    add

---
+++ Assets/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	public int speed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other);
+		if (other.gameObject.tag == "enemy") {
+			Destroy(other.gameObject);
+			Destroy(this.gameObject);
+		
+		}
+	}
+}
---
+++ Assets/shooter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class shooter : MonoBehaviour {
+	public bool saw;
+	public bool inrange;
+	public Transform blockjes;
+	public GameObject bullet;
+	private Transform Target;
+	public List<Transform> targets = new List<Transform>();
+	
+	public enum States
+	{
+		patrolState,
+		chaseState,
+		attackState		
+	}
+	
+	public States state;
+
+	void Start () {
+		switch (state) {
+		case States.patrolState:
+			Debug.Log ("Patrol");
+			break;
+		case States.chaseState:
+			Debug.Log ("Chase");
+			break;
+		case States.attackState:
+			break;
+		}
+	}
+	
+	void Update () {
+		transform.LookAt (Target);
+
+		if (state == States.patrolState && saw == true) {
+			state = States.chaseState;
+
+		}
+		else if (state == States.chaseState && inrange == true) {	
+			state = States.attackState;
+		}
+		else if(state == States.attackState)
+		{
+			shoot();
+			state = States.patrolState;
+		}
+
+
+
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "enemy") {
+			Target = other.transform;
+			targets.Add(Target);
+		}
+	}
+	
+	void OnTriggerExit(Collider other){
+		if (other.tag == "enemy") {
+			Target = other.transform;
+			targets.Remove(Target);
+		}
+	}
+
+	void shoot(){
+		Instantiate(bullet, transform.position, transform.rotation);
+	}
+}
